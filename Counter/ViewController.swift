d2de7996c9994a5152b222b6ab09ed432bee42b7
Arina <arina.kolganova21@gmail.com>
2023-07-28 02:19:42 +1000

    Сделана базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clickButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        clickButton.tintColor = .black
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped() {
+        countLabel.text = String(Int(countLabel.text!)! + 1)
+    }
+    
 }
 
