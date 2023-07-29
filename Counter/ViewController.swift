a3932d1afa6456ef697305984e7513d35b81a1f2
Arina <arina.kolganova21@gmail.com>
2023-07-29 15:22:07 +1000

    1) поставлен модификатор доступа самый закрытый для переменных и функций 2) был убран комментарийн во viewDidLoad 3) произошел rename методов

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,14 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    @IBOutlet weak var logTextView: UITextView!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var nullButton: UIButton!
-    @IBOutlet weak var countLabel: UILabel!
-    
     private var dateFormatter = DateFormatter()
+    
+    @IBOutlet private weak var logTextView: UITextView!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var nullButton: UIButton!
+    @IBOutlet private weak var countLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +21,14 @@ class ViewController: UIViewController {
         plusButton.tintColor = .red
         nullButton.tintColor = .lightGray
         logTextView.isEditable = false
-        
-        // Do any additional setup after loading the view.
     }
     
-    @IBAction func countNull(_ sender: Any) {
+    @IBAction private func refreshButtonTapped(_ sender: Any) {
         countLabel.text = "0"
         logTextView.text.append("\(dateFormatter.string(from: Date())): значение сброшено\n")
     }
     
-    @IBAction func countDel(_ sender: Any) {
+    @IBAction private func minusButtonTapped(_ sender: Any) {
         if (Int(countLabel.text!)! > 0) {
             countLabel.text = String(Int(countLabel.text!)! - 1)
             logTextView.text.append("\(dateFormatter.string(from: Date())): значение изменено на -1\n")
@@ -40,7 +36,8 @@ class ViewController: UIViewController {
             logTextView.text.append("\(dateFormatter.string(from: Date())): попытка уменьшить значение счётчика ниже 0\n")
         }
     }
-    @IBAction func countAdd(_ sender: Any) {
+    
+    @IBAction private func plusButtonTapped(_ sender: Any) {
         countLabel.text = String(Int(countLabel.text!)! + 1)
         logTextView.text.append("\(dateFormatter.string(from: Date())): значение изменено на +1\n")
     }
